d08b225b4d83f0d8532685d1363308a51e9f669d
VerkhoturovIP <verkhoturov314@gmail.com>
2025-03-10 19:01:43 +0300

    fonts

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from 'next/app';
+import clsx from 'clsx';
+import { fixelDisplayFont, fixelTextFont } from '@/shared/fonts';
 import { Providers } from './providers';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <Providers>
-            <Component {...pageProps} />
+            <div className={clsx(fixelDisplayFont.className, fixelTextFont.className)}>
+                <Component {...pageProps} />
+            </div>
         </Providers>
     );
 }
