815f8fd5b629f8beae09eaa43ca3927a8808f0e2
VerkhoturovIP <verkhoturov314@gmail.com>
2025-03-12 23:11:53 +0300

    fix build #4

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,7 +1,7 @@
 export { SubmitButton } from './SubmitButton';
 export { Field } from './Field';
-export { Card } from './Card';
-export { Logo } from './Logo';
+export { Card } from './Card/index';
+export { Logo } from './Logo/index';
 export * from './Icons';
 export { Textarea } from './Textarea';
 export { Loader } from './Loader';
